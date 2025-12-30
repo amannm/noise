c84a9134600ac98f04e8cf5931c77d5d9d819a8a
Amann Malik <amannmalik@gmail.com>
2025-12-30 17:57:35 -0600

    initial

--- convert.sh
+++ convert.sh
@@ -2,4 +2,5 @@
 set -eu
 
 ffmpeg -i ac_on.m4a  -ac 1 -ar 16000 ac_on.wav
-ffmpeg -i ac_off.m4a -ac 1 -ar 16000 ac_off.wav
+ffmpeg -i fridge_on.m4a  -ac 1 -ar 16000 fridge_on.wav
+ffmpeg -i all_off.m4a -ac 1 -ar 16000 all_off.wav
